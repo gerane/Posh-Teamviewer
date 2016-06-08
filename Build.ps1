211b7445c98fe1d8d93df355b94fc7a5a3865a5c
Brandon Padgett <gerane@gmail.com>
2016-06-07 19:54:22 -0500

    Added PlatyPS to build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,8 @@
 # Grab nuget bits, install modules, set build variables, start build.
 $Null = Get-PackageProvider -Name NuGet -ForceBootstrap
 
-Install-Module Psake, PSDeploy, Pester, BuildHelpers, PSScriptAnalyzer -force
-Import-Module Psake, BuildHelpers
+Install-Module Psake, PSDeploy, Pester, BuildHelpers, PlatyPS, PSScriptAnalyzer -force
+Import-Module Psake, BuildHelpers, PlatyPS, PSDeploy, Pester
 
 Set-BuildEnvironment
 
