cb38b28b76679f3400b4d97e5cc4e9c807e800cd
Brandon Padgett <gerane@gmail.com>
2016-06-08 15:10:25 -0500

    Updated Link Test to show File names

--- Tests/Module.Links.Tests.ps1
+++ Tests/Module.Links.Tests.ps1
@@ -1,24 +1,32 @@
 ﻿#TODO: Convert to PoshSpec
 
 $Links = @()
-$MarkdownLinks = Get-ChildItem $PSScriptRoot\..\* -Include '*.md' -Recurse | Select-String '^.*\[.*\]\((http.*)\).*$' -AllMatches
+$MarkdownLinks = gci 'C:\Github\Posh-Teamviewer\*' -Include '*.md' -Recurse | Select-String '^.*\[.*\]\((http.*)\).*$' -AllMatches
 foreach ($Match in $MarkdownLinks)
 {
-    $Links += $Match.Matches.Groups.value[1]
+    $Links += @{$Match.Matches.Groups.value[1] = $Match.Path}
 }
 
-$Links = $Links | Sort-Object -Unique
 
+$Files = $Links.Values | Sort-Object -Unique
+    
+    
 Describe "Testing Resources" {
     
-    Context "Links are working" {
-        
-        foreach ($Link in $Links)
-        {
-            It "[$($Link)] should have 200 Status Code" {
+    foreach ($File in $Files)
+    {
+        Context "Testing Links: [$($File)]" {
+            $FileLinks = $Links | Where-Object { $_.Values -eq $File}
 
-                $Results = Invoke-WebRequest -Uri $Link -UseBasicParsing
-                $Results.StatusCode | Should Be '200'
+            foreach ($Link in $FileLinks)
+            {
+                $Link = ($Link.Keys | Out-String).Trim()
+                
+                It "[$($Link)] should have 200 Status Code" {
+                
+                    $Results = Invoke-WebRequest -Uri $Link -UseBasicParsing
+                    $Results.StatusCode | Should Be '200'
+                }
             }
         }
     }
--- docs/index.md
+++ docs/index.md
@@ -2,6 +2,6 @@
 
 Posh-TeamViewer Documentation is using a combination of [PlatyPS](https://github.com/PowerShell/platyPS) and [ReadTheDocs](https://readthedocs.org/). This let's you right the command help once and it will create External Help, Github Markdown Help, and Help hosted on ReathTheDocs. 
 
-- Help is written in Markdown in [the docs folder](https://github.com/gerane/Posh-Teamviewer/tree/dev/docs) following a PlatyPS schema.
+- Help is written in Markdown in [the docs folder](https://github.com/gerane/Posh-Teamviewer/tree/Dev/docs) following a PlatyPS schema.
 - It then gets exported into an External Help file [Posh-Teamviewer-help.xml](https://github.com/gerane/Posh-Teamviewer/blob/Dev/Posh-Teamviewer/en-US/Posh-Teamviewer-help.xml). 
-- When committed, ReadTheDocs then uses the [mkdocs.yml](https://github.com/RamblingCookieMonster/PSDeploy/blob/dev/mkdocs.yml) file to build the Documentation based on the mkdocs.yml layout.
+- When committed, ReadTheDocs then uses the [mkdocs.yml](https://github.com/gerane/Posh-Teamviewer/blob/Dev/mkdoc.yml) file to build the Documentation based on the mkdoc.yml layout.
